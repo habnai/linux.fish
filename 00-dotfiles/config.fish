362cd05071c72407958fc1a88173c12b52129de8
habnai <habnai@inmaily.com>
2022-08-27 17:10:13 +0200

    update

--- 00-dotfiles/config.fish
+++ 00-dotfiles/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    echo "Your session is configured by dotfiles."
-    echo "  see also: https://github.com/habnai/linux.fish"
 end
 
 fish_add_path $DOTFILES/bin $HOME/.bin $HOME/.local/bin
