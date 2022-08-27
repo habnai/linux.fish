6c2bd2a4957fea5af9a446960f3b7a94c4e8e18f
habnai <habnai@inmaily.com>
2022-08-27 19:01:15 +0200

    update

--- fd/install.fish
+++ fd/install.fish
@@ -3,5 +3,5 @@
 # on ubuntu, fd is installed as fdfind instead of fd, this links it to a
 # local folder which is in PATH.
 if command -qs fdfind
-	ln -sf (which fdfind) ~/.bin/fd
+	ln -sf (which fdfind) ~/.local/bin/fd
 end
